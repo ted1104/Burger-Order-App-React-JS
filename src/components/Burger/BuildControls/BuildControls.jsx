f15a96981907675c2d55f39c7899f0759dbb9cf9
Teddy Walter <teddywalter2016@gmail.com>
2021-07-15 19:12:06 +0200

    adding builder control buttons

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,4 +1,32 @@
 import React from "react";
-const buildControls = (props) => <div></div>;
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  {
+    label: "Salad",
+    type: "salad",
+  },
+  {
+    label: "Bacon",
+    type: "bacon",
+  },
+  {
+    label: "Cheese",
+    type: "cheese",
+  },
+  {
+    label: "Meat",
+    type: "meat",
+  },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((item, i) => (
+      <BuildControl key={item.label} />
+    ))}
+  </div>
+);
 
 export default buildControls;
