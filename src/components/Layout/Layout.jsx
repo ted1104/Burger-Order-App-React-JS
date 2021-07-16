fbe6b7a009dd34bdbc93bab2bdd57fcf255b0b0c
Teddy Walter <teddywalter2016@gmail.com>
2021-07-16 02:56:59 +0200

    open side bar menu on click on men

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,15 +6,20 @@ import SideDrawer from "../Navigation/NavigationItems/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
   _sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
+  _sideDrawerOpenHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerClicked={this._sideDrawerOpenHandler} />
         <SideDrawer
           closed={this._sideDrawerClosedHandler}
           open={this.state.showSideDrawer}
---
+++ src/components/Navigation/NavigationItems/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const drawerToggle = (props) => <div onClick={props.clicked}>MENu</div>;
+export default drawerToggle;
