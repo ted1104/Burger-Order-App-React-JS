3c43b90a2077aa35818013d9ea81a06de7bb9784
Teddy Walter <teddywalter2016@gmail.com>
2021-07-16 02:20:17 +0200

    side bar mobile done!

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Auxiliaire";
 import classe from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/NavigationItems/SideDrawe/SideDrawe";
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classe.content}>{props.children}</main>
   </Aux>
 );
