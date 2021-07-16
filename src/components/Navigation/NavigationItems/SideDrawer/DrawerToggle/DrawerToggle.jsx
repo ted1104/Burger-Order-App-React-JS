7519e9af1b264c8c719d4a41fe71285117f9f6e7
Teddy Walter <teddywalter2016@gmail.com>
2021-07-16 02:59:23 +0200

    humbuger icon in css

--- src/components/Navigation/NavigationItems/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ src/components/Navigation/NavigationItems/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import classes from "./DrawerToggle.module.css";
 
-const drawerToggle = (props) => <div onClick={props.clicked}>MENu</div>;
+const drawerToggle = (props) => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 export default drawerToggle;
