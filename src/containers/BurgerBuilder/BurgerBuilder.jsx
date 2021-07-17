3878c27269a78c5a4695614c9412079562487e48
Teddy Walter <teddywalter2016@gmail.com>
2021-07-17 23:22:44 +0200

    hoc to handler global axios error

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -55,7 +56,7 @@ class BurgerBuilder extends Component {
     axios
       .post("/orders.json", orders)
       .then((response) => {
-        this.setState({ loading: false, purchasing: false  });
+        this.setState({ loading: false, purchasing: false });
         console.log(response);
       })
       .catch((err) => {
@@ -165,4 +166,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
